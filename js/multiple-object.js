b83c086931283acc639256dff7e123d2e8f9ff77
hung247.dh <hung247.dh@gmail.com>
2022-08-02 18:14:26 +0700

    hung

---
+++ js/multiple-object.js
@@ -0,0 +1,19 @@
+function Hotel (name, rooms, booked) { 
+    this.name = name; 
+    this.rooms = rooms; 
+    this.booked = booked; 
+    this.checkAvailability = function() { 
+        return this.rooms - this.booked;
+    }; 
+}var quayHotel = new Hotel('Quay', 40, 25);
+var parkHotel = new Hotel('Park', 120, 77);
+
+var details1 = quayHotel.name + 'rooms';
+    details1 += quayHotel.checkAvailability(); 
+var elHotell = document.getElementById('hotel1'); 
+elHotel1.textContent = details1; 
+
+var details2 = parkhotel.name + 'rooms';
+    details2 += parkHotel.checkAvailability(); 
+var elHotel2 = document.getElementById('hotel2');
+elHotel2.textContent = details2;
